fd70f88f276f642729652a2349cb4e8bee1b76c9
Matthew Chang <monkeymatt2023@gmail.com>
2022-09-20 13:28:43 -0700

    Commit Constructor

--- src/Commit.java
+++ src/Commit.java
@@ -3,6 +3,7 @@ import java.util.*;
 import java.io.*;
 import java.math.BigInteger;
 import java.security.MessageDigest;
+import java.time.LocalDateTime;
 
 public class Commit {
 
@@ -12,18 +13,40 @@ public class Commit {
 	private static String summary;
 	private static String author;
 	private static String date;
+	private static String fileName;
 	
-	public Commit(String pointer, String s, String a) {
+	public Commit(String pointer, String s, String a, String p) throws FileNotFoundException {
 		pTree = pointer;
-		parent = null;
-		other = null;
 		summary = s;
 		author = a;
-		date = getDate();
+		date = "" + LocalDateTime.now();
+		if (p != null)
+			parent = p;
+		else
+			parent = null;
+		other = null;
+		
+		fileName = getSHA1();
+		
+		if (parent != null) {
+			Scanner input = new Scanner(new File("test/objects/" + parent));
+			String contents = "";
+			contents += input.nextLine() + "\n";
+			contents += input.nextLine() + "\n";
+			contents += fileName + "\n";
+			input.nextLine();
+			contents += input.nextLine() + "\n";
+			contents += input.nextLine() + "\n";
+			contents += input.nextLine() + "\n";
+			
+			PrintWriter pw = new PrintWriter("test/objects/" + parent);
+			pw.append(contents);
+			pw.close();
+		}
 	}
 	
-	private String getSHA1 (String s1){
-		String value = s1;
+	private String getSHA1(){
+		String value = "" + summary + date + author + parent;
 		String output = "";
 
 		try {
@@ -42,7 +65,16 @@ public class Commit {
 		return date;
 	}
 	
-	public void writeFile() {
-		
+	public void writeFile() throws FileNotFoundException {
+		PrintWriter pw = new PrintWriter("test/objects/" + new File(fileName));
+		pw.append(pTree + "\n");
+		if (parent != null)
+			pw.append(parent + "\n");
+		pw.append(other + "\n");
+		pw.append(author + "\n");
+		pw.append(date + "\n");
+		pw.append(summary + "\n");
+		pw.close();
 	}
+
 }
